4c34f4c462a645ec7c78149997996a61dcb7b3e7
Fadi <fadeyaseen3@gmail.com>
2025-04-12 12:18:21 +0300

    test: after modifications

---
+++ scripts/deploy.sh
@@ -0,0 +1,2 @@
+echo "Service 1 running on http://localhost:5000"
+echo "Service 2 running on http://localhost:5001"
---
+++ scripts/test.sh
@@ -0,0 +1,5 @@
+echo "Running tests"
+
+pytest microservices/service1/tests
+pytest microservices/service2/tests
+
