61050017ccc2d34b6d2702256b602fb3f65a2ba1
Fadi <fadeyaseen3@gmail.com>
2025-04-12 12:59:32 +0300

    fix: fixed import

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,9 @@ echo "Running tests"
 
 cd "$(dirname "$0")/.." || exit 1
 
+
+export PYTHONPATH=$(pwd)
+
 PYTHONPATH=$(pwd)/microservices/service1 pytest microservices/service1/tests
 PYTHONPATH=$(pwd)/microservices/service2 pytest microservices/service2/tests
 
